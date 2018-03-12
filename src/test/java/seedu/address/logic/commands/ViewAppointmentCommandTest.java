6660601563f5b861e9e1b7e6fe85abefa9825ed6
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-12 22:37:09 +0800

    add ViewAppointmentCommandTest class

---
+++ src/test/java/seedu/address/logic/commands/ViewAppointmentCommandTest.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import org.junit.Before;
+
+public class ViewAppointmentCommandTest {
+
+    private ViewAppointmentCommand viewAppointmentCommand;
+
+    @Before
+    public void setUp() {
+        viewAppointmentCommand = new ViewAppointmentCommand();
+    }
+}
