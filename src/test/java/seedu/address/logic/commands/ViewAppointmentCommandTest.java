8493c1898ea8cfc76d1b184ac1e71d6aaa71d3d7
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-13 21:42:19 +0800

    add typical appointments class & appointment builder class

--- src/test/java/seedu/address/logic/commands/ViewAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewAppointmentCommandTest.java
@@ -2,12 +2,17 @@ package seedu.address.logic.commands;
 
 import org.junit.Before;
 
+import seedu.address.model.Model;
+
 public class ViewAppointmentCommandTest {
 
+    private Model model;
+    private Model expectedModel;
     private ViewAppointmentCommand viewAppointmentCommand;
 
     @Before
     public void setUp() {
+
         viewAppointmentCommand = new ViewAppointmentCommand();
     }
 }
---
+++ src/test/java/seedu/address/testutil/TypicalAppointments.java
@@ -0,0 +1,8 @@
+package seedu.address.testutil;
+
+/**
+ * A utility class containing a list of {@code Appointment} objects to be used in tests.
+ */
+public class TypicalAppointments {
+    public static final
+}
