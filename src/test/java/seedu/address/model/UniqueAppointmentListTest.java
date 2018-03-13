8327a871ac4072b9ff80b1b507200b38461a6e27
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-13 22:16:57 +0800

    add unique appointment list test

---
+++ src/test/java/seedu/address/model/UniqueAppointmentListTest.java
@@ -0,0 +1,19 @@
+package seedu.address.model;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.model.appointment.UniqueAppointmentList;
+
+public class UniqueAppointmentListTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void asObservableList_modifyList_throwsUnsupportOperationException() {
+        UniqueAppointmentList uniqueAppointmentListList = new UniqueAppointmentList();
+        thrown.expect(UnsupportedOperationException.class);
+        uniqueAppointmentListList.asObservableList().remove(0);
+    }
+}
