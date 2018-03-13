9ac52841f7093244ba8ef754b8f5dc6ff2bde0fd
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-13 18:17:46 +0800

    add xmladapterappointment class

---
+++ src/main/java/seedu/address/storage/XmlAdaptedAppointment.java
@@ -0,0 +1,61 @@
+package seedu.address.storage;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlValue;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.Appointment;
+
+/**
+ * JAXB-friendly adapted version of the Appointment.
+ */
+public class XmlAdaptedAppointment {
+
+    @XmlValue
+    private String patientName;
+
+    @XmlValue
+    private String dateTime;
+
+    /**
+     * Constructs an XmlAdaptedAppointment
+     * This is the no-arg constructor that is required by JAXB.
+     */
+    public XmlAdaptedAppointment() {}
+
+    /**
+     * Constructs a {@code XmlAdaptedAppointment} with the given {@code patientName} and {@code dateTime}.
+     */
+    public XmlAdaptedAppointment(String patientName, Date dateTime) {
+        this.patientName = patientName;
+        this.dateTime = dateTime.toString();
+    }
+
+    /**
+     * converts a given Appointment into this class for JAXB use.
+     *
+     * @param source future changes to this will not affect the created
+     */
+    public XmlAdaptedAppointment(Appointment source) {
+        this.patientName = source.getPatientName();
+        this.dateTime = source.getAppointmentDateTime().toString();
+    }
+
+    public Appointment toModelType() throws IllegalValueException {
+        return new Appointment(patientName, new Date());
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof XmlAdaptedAppointment)) {
+            return false;
+        }
+        
+        return (patientName.equals(((XmlAdaptedAppointment) other).patientName)
+                && dateTime.equals(((XmlAdaptedAppointment) other).dateTime));
+    }
+}
