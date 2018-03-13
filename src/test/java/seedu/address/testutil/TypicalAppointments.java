53a40559175ef9e445071155c432fdacb09bfbc4
Kyholmes <karen_tankaiyong@hotmail.com>
2018-03-13 21:50:08 +0800

    modified appointment builder to build appt obj

--- src/test/java/seedu/address/testutil/TypicalAppointments.java
+++ src/test/java/seedu/address/testutil/TypicalAppointments.java
@@ -4,5 +4,5 @@ package seedu.address.testutil;
  * A utility class containing a list of {@code Appointment} objects to be used in tests.
  */
 public class TypicalAppointments {
-    public static final
+
 }
